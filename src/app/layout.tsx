a09d4d0cf82d373fbabf78e8c60c8732de1ba92b
syneju <syneju@gmail.com>
2024-06-02 00:47:53 +0800

    update next 15

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+import "./_supressLogs";
 
-const inter = Inter({ subsets: ["latin"] });
+const geistSans = localFont({
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+});
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -16,7 +24,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				{children}
+			</body>
 		</html>
 	);
 }
